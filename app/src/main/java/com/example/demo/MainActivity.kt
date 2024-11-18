2ca5496ce171185985b2a792d4a38c60bce81bd3
Xavier <6195429@cegepoutaouais.qc.ca>
2024-11-18 12:59:56 -0500

    sfbadngmhdmghdmgjd

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.demo
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import org.json.JSONObject
 import java.io.DataOutputStream
 import java.net.HttpURLConnection
@@ -22,12 +33,22 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 
+val CHANNEL_ID = "Channel1"
+
 class MainActivity : ComponentActivity() {
     val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        creerCanal()
+        demanderPermissionNotification()
+
+        val titre = "Ma notification"
+        val texte = "Bonjour le monde"
+        val notificationID = 1
+        afficherNotification(notificationID, titre, texte)
 
         executerAsynchrone()
 
@@ -135,6 +156,51 @@ class MainActivity : ComponentActivity() {
             }
         }
         thread.start()
+    }
+
+    private fun demanderPermissionNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (!result) {
+                    Toast.makeText(this, "La permission n'a pas été accordée", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PermissionChecker.PERMISSION_GRANTED) {
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private fun creerCanal() {
+        val channelName = "Channel1"
+        val channelDescription = "Canal de notifications"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, channelName, importance).apply {
+                description = channelDescription
+            }
+            val notificationManager = applicationContext.getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
+
+    private fun afficherNotification(id : Int, titre: String, texte: String) {
+        val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.star_on)
+            .setContentTitle(titre)
+            .setContentText(texte)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(applicationContext)) {
+            if (ActivityCompat.checkSelfPermission(
+                applicationContext,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                notify(id, builder.build())
+            }
+        }
     }
 }
 
